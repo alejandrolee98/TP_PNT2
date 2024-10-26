e771f7b54a393f63e1f2fc45121e5d1967539ee8
alejandrolee98 <ale.nico230398@gmail.com>
2024-10-26 17:42:23 -0300

    cambio de carpeta components a views

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../components/Home.vue';
-import Cotizador from '../components/Cotizador.vue';
-import Perfil from '../components/Perfil.vue';
-import Login from '../components/Login.vue';
+import cotizador from '../views/cotizador.vue';
+import perfil from '../views/perfil.vue';
+import login from '../views/login.vue';
 
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/cotizador', name: 'Cotizador', component: Cotizador },
-    { path: '/perfil', name: 'Perfil', component: Perfil },
-    { path: '/login', name: 'Login', component: Login },
+    { path: '/cotizador', name: 'cotizador', component: cotizador },
+    { path: '/perfil', name: 'Perfil', component: perfil },
+    { path: '/login', name: 'Login', component: login },
 ];
 
 const router = createRouter({
