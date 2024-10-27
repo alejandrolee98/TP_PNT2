a348aff927bbd08376d43fbc68cfb460653bb116
Facundo <facundocayita@hotmail.com>
2024-10-27 01:27:00 -0300

    Seteo inicial Pinia para Login y agregado de estetica vista Home

--- src/App.vue
+++ src/App.vue
@@ -15,3 +15,13 @@
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue';
 </script>
+
+<style>
+.bg-image {
+    background-image: url('@/images/tu-imagen.png'); /* Cambia 'tu-imagen.png' por el nombre real de tu imagen */
+    background-repeat: repeat; /* Repetir la imagen */
+    background-size: auto; /* Ajustar automáticamente el tamaño */
+    height: 100%; /* Asegúrate de que el contenedor tenga altura completa */
+    margin: 0; /* Eliminar márgenes */
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,38 @@
 <template>
-    <div class="container">
-        <h1>Home</h1>
-        <p>La home</p>
+    <div class="container text-center my-5">
+        <h1 class="display-4 mb-4">Bienvenido a nuestro Calculador de Proyectos de Impresión 3D</h1>
+        <p class="lead mb-4">Aquí podrás calcular el costo y el tiempo de impresión de tus proyectos.</p>
+        
+        <router-link to="/cotizador" class="btn btn-secondary btn-lg">Ir al Cotizador</router-link>
     </div>
 </template>
+
+<style scoped>
+.container {
+    background-color: #0e0d0c; /* Color de fondo claro */
+    border-radius: 8px; /* Bordes redondeados */
+    box-shadow: 0 4px 8px rgba(134, 130, 130, 0.2); /* Sombra suave */
+    padding: 40px; /* Espaciado interno */
+    background-image: url('../images/pacman.png'); /* Cambia la ruta a la imagen que elijas */
+    background-repeat: repeat; /* Repetir la imagen */
+    background-size: 130px 130px; /* Ajustar automáticamente el tamaño */
+}
+
+body {
+    background-image: url('../images/Logo_Naranja.jpeg'); /* Cambia la ruta a la imagen que elijas */
+    background-repeat: repeat; /* Repetir la imagen */
+    background-size: auto; /* Ajustar automáticamente el tamaño */
+}
+
+h1 {
+    color: #ff7b00; /* Color del encabezado */
+    font-weight: bold; /* Hacer el texto en negrita */
+    text-shadow: 2px 2px 4px rgba(18, 17, 17, 0.975); /* Sombra de texto */
+}
+
+p {
+    color: #fffafa; /* Color del texto */
+    font-weight: bold; /* Hacer el texto en negrita */
+    text-shadow: 1px 1px 3px rgba(3, 3, 3, 0.7); /* Sombra de texto */
+}
+</style>
