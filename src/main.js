29b217cd04579f66dfec7e96e0dd894284cb79b1
alejandrolee98 <ale.nico230398@gmail.com>
2024-10-29 21:42:25 -0300

    instancia de pinia

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 import router from './routes/router.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
