eb6b84b01f2e64187b68453a693269aaba08cc2c
alejandrolee98 <ale.nico230398@gmail.com>
2024-10-29 21:20:28 -0300

    detalle proyecto view

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import Home from '../components/Home.vue';
 import cotizador from '../views/cotizador.vue';
 import perfil from '../views/perfil.vue';
 import login from '../views/login.vue';
+import detalleProyecto from '../views/detalleProyecto.vue';
 
 
 const routes = [
@@ -11,6 +12,7 @@ const routes = [
     { path: '/cotizador', name: 'cotizador', component: cotizador },
     { path: '/perfil', name: 'Perfil', component: perfil },
     { path: '/login', name: 'Login', component: login },
+    { path: '/detalleProyecto', name: 'Detalle', component: detalleProyecto },
 ];
 
 const router = createRouter({
---
+++ src/views/detalleProyecto.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+      <h2>Detalle del Proyecto</h2>
+      <dl class="row details-info">
+        <dt class="col-sm-3">Descripción</dt>
+        <dd class="col-sm-9">Proyecto 1</dd>
+        
+        <dt class="col-sm-3">Ancho</dt>
+        <dd class="col-sm-9">5 cm</dd>
+        
+        <dt class="col-sm-3">Alto</dt>
+        <dd class="col-sm-9">5 cm</dd>
+        
+        <dt class="col-sm-3">Grosor</dt>
+        <dd class="col-sm-9">5 cm</dd>
+        
+        <dt class="col-sm-3">Cantidad de Colores</dt>
+        <dd class="col-sm-9">3</dd>
+        
+        <dt class="col-sm-3">Cantidad de Unidades</dt>
+        <dd class="col-sm-9">1</dd>
+        
+        <dt class="col-sm-3">Tipo</dt>
+        <dd class="col-sm-9">Iman</dd>
+        
+        <dt class="col-sm-3">Costo Unitario</dt>
+        <dd class="col-sm-9"><strong>$234</strong></dd>
+        
+        <dt class="col-sm-3">Costo Total</dt>
+        <dd class="col-sm-9"><strong>$234</strong></dd>
+      </dl>
+      <div class="text-center">
+        <router-link class="nav-link" to="/cotizador"><button class="btn btn-secondary">Volver al Cotizador</button></router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style scoped>
+  .container {
+    max-width: 800px;
+    margin: auto;
+    padding: 20px;
+    background-color: #f8f9fa;
+    border-radius: 20px;
+    box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  }
+  .details-info dt {
+    font-weight: bold;
+  }
+  .details-info dd {
+    margin-bottom: 10px;
+  }
+  </style>
