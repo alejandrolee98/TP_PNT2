395e9fbb59e22956c95c2395e559f1bd721a5c16
alejandrolee98 <ale.nico230398@gmail.com>
2024-11-05 22:44:54 -0300

    vista de perfil

--- src/views/perfil.vue
+++ src/views/perfil.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="container">
         <div class="card">
-            <div class="card-header">
-                <h1>Bienvenido Nombre!</h1> <!--agregar el nombre recuperando el id-->
+            <div class="card-header" v-if="authStore.isAuthenticated">
+                <h1>Bienvenido {{authStore.user.nombre}}</h1> <!--agregar el nombre recuperando el id-->
             </div>
             <div class="card-body">
                 <!--agregar datos recuperando el id-->
-                <h6>Nro de cliente: Nro</h6>
-                <h6>Nombre: nombre</h6>
-                <h6>Apellido: apellido</h6>
-                <h6>Email: email</h6>
-                <h6>Dirección: direccion</h6>
-                <h6>Localidad: localidad</h6>
-                <h6>CP: codigo postal</h6>
-                <h6>Provincia: provincia</h6>
+                <h6>Nro de cliente: {{authStore.user.id}}</h6>
+                <h6>Nombre: {{authStore.user.nombre}}</h6>
+                <h6>Apellido: {{authStore.user.apellido}}</h6>
+                <h6>Email: {{authStore.user.email}}</h6>
+                <h6>Dirección: {{authStore.user.direccion}}</h6>
+                <h6>Localidad: {{authStore.user.localidad}}</h6>
+                <h6>CP: {{authStore.user.cp}}</h6>
+                <h6>Provincia: {{authStore.user.provincia}}</h6>
                 <button href="#" class="btn btn-primary">Editar datos</button>
             </div>
         </div>
     </div>
 </template>
 <script setup>
+import { useAuthStore } from '../stores';
+const authStore = useAuthStore();
 </script>
