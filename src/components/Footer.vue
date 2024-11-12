df5c858b411263ac44024856400ef1d857aab226
unknown <agustinakraemer@gmail.com>
2024-11-12 19:26:58 -0300

    cambio de color del footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,19 +3,22 @@
         <div class="container">
             <p>&copy; 2024 Plastic Monkey. Todos los derechos reservados.</p>
             <ul class="list-inline">
-                <li class="list-inline-item"><a href="#" class="text-black">Política de privacidad</a></li>
-                <li class="list-inline-item"><a href="#" class="text-black">Términos de uso</a></li>
-                <li class="list-inline-item"><a href="#" class="text-black">Contacto</a></li>
+                <li class="list-inline-item"><a href="#" class="text-white">Política de privacidad</a></li>
+                <li class="list-inline-item"><a href="#" class="text-white">Términos de uso</a></li>
+                <li class="list-inline-item"><a href="#" class="text-white">Contacto</a></li>
             </ul>
         </div>
     </footer>
 </template>
 
 <style scope>
-footer{
+footer {
     background-color: black;
     color: white;
     position: relative;
     font-size: 0.8rem;
+}
+.list-inline-item {
+    color: white;
 }
 </style>
